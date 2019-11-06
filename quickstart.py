430e5dec16ec02c77d81fa18f7a3d158a2447261
Sabry Razick <sabryr@gmail.com>
2019-11-06 13:59:22 +0100

    Correct document id

--- quickstart.py
+++ quickstart.py
@@ -24,7 +24,7 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
 
 # The ID of a sample document.
-DOCUMENT_ID = '1VQ4lUr1hrrKQwOAYa2z0h-0xltBm1PEuQLY8VksWk7'
+DOCUMENT_ID = '1VQ4lUr1hrrKQwOAYa2z0h-0xltBm1PEuQLY8VksWk7c'
 
 def main():
     """Shows basic usage of the Docs API.
